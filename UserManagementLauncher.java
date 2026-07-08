1f9145966eda603f7267f4f8946085d6423f9b90
Mmbone Victoria Blasse <blassevictoria21@gmail.com>
2026-07-08 16:03:07 +0300

    Delete UserManagementLauncher.java

--- UserManagementLauncher.java
+++
@@ -1,5 +0,0 @@
-public class UserManagementLauncher {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new UserManagementPage("admin", "ADMIN"));
-    }
-}
